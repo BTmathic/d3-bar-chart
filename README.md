b027a1e8623d900a5d7b763ec2a959d54fa5739a
Alexander Molnar <mathic@gmail.com>
2018-05-21 01:15:15 -0400

    Refactored code for clarity

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 
 Using [Federal Reserve economic data](https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json) we display the quarterly GDP data for the United States from 1947 to 2015 in an interactive chart.
 
-## Tools used
+### Front end
+
+* Javascript
+
+### Data Visualization
 
 * D3
+
+### Build Tools
+
 * Webpack
+* Yarn
+* Babel
--- src/app.js
+++ src/app.js
@@ -5,29 +5,50 @@ import * as d3 from 'd3';
 fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json').then((response) => {
   return response.json();
 }).then((data) => {
+  let margin;
+  let height, width;
+
+  let svg = d3.select('#container').append('svg');
+  let popup = d3.select('body').append('div');
+  let description = d3.select('#container').append('div');
+  let x,y;
+  
   const gdpData = data.data;
   const gdpAmounts = data.data.map((entry) => entry[1]);
   const gdpDates = data.data.map((entry) => entry[0]);
+  let barWidth;
 
-  const margin = { top: 20, right: 20, bottom: 20, left: 75};
-  const width = 865 - margin.left - margin.right;
-  const height = 450 - margin.top - margin.bottom;
+  main();
 
-  const x = d3.scaleTime().range([0, width]);
-  const y = d3.scaleLinear().range([height, 0]);
+  function main() {
+    popup.attr('class', 'tooltip')
+      .style('opacity', 0);
 
-  const barWidth = width / gdpAmounts.length;
+    setSizes();
+    drawGraph();
+  }
 
-  let svg = d3.select('#container').append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+  function setSizes() {
+    margin = { top: 20, right: 20, bottom: 20, left: 75 };
+    height = 450 - margin.top - margin.bottom;
+    width = 865 - margin.left - margin.right;
+    barWidth = width / gdpAmounts.length;
 
-  x.domain([new Date(gdpDates[0]), new Date(gdpDates[gdpDates.length - 1])]);
-  y.domain([0, d3.max(gdpAmounts)]);
+    x = d3.scaleTime().range([0, width]);
+    y = d3.scaleLinear().range([height, 0]);
 
-  svg.append('g')
+    svg
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    x.domain([new Date(gdpDates[0]), new Date(gdpDates[gdpDates.length - 1])]);
+    y.domain([0, d3.max(gdpAmounts)]);
+  }
+
+  function drawGraph() {
+    svg.append('g')
     .attr('transform', `translate(0, ${height})`)
     .call(d3.axisBottom(x).tickFormat((date) => d3.timeFormat('%Y')(date)));
 
@@ -41,6 +62,53 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
     .attr('dy', '0em')
     .style('text-anchor', 'middle')
     .text('GDP (in billions)')
+  
+  svg.append('g')
+    .selectAll('rect')
+    .data(gdpData)
+    .enter()
+    .append('rect')
+    .attr('fill', 'steelblue')
+    .attr('width', barWidth)
+    .attr('height', (d) => height*d[1] / d3.max(gdpAmounts, (amount) => amount))
+    .attr('transform', (d, i) => 'translate(' + i * barWidth + ',' + height + ')' + 'scale(1, -1)')
+    .on('mouseover', (d) => mouseover(d))
+    .on('mouseout', mouseout);
+
+  description
+    .attr('class', 'description')
+    .style('max-width', '925px')
+    .style('font-size', '12px')
+    .style('padding-left', '15px')
+    .style('text-align', 'center')
+
+  description.append("text")
+    .text(data.description);
+  }
+
+  function mouseover(d) {
+    popup.transition()
+      .duration(200)
+      .style('opacity', 0.9);
+    const rect = d3.select(d);
+    const rectDate = rect._groups[0][0][0].split('-');
+    const rectAmount = rect._groups[0][0][1].toString().split('.');
+    let amount;
+    if (rectAmount.length === 1) {
+      amount = '$' + rectAmount[0].concat('.00');
+    } else {
+      amount = '$' + rectAmount[0].concat('.' + rectAmount[1]).concat('0');
+    }
+    popup.html(`<div id='amount'>${amount} Billion</div><div id='date'>${getDate(rectDate[1])}, ${rectDate[0]}</div>`)
+      .style('left', (d3.event.pageX + 5) + 'px')
+      .style('top', (d3.event.pageY - 50) + 'px')
+  }
+
+  function mouseout() {
+    popup.transition()
+      .duration(500)
+      .style('opacity', 0);
+  }
 
   const getDate = (month) => {
     switch (month) {
@@ -54,50 +122,4 @@ fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/maste
         return 'October';
     }
   }
-
-  const popup = d3.select('body').append('div')
-    .attr('class', 'tooltip')
-    .style('opacity', 0);
-  
-  svg.append('g')
-    .selectAll('rect')
-    .data(gdpData)
-    .enter()
-    .append('rect')
-    .attr('fill', 'steelblue')
-    .attr('width', barWidth)
-    .attr('height', (d) => height*d[1] / d3.max(gdpAmounts, (amount) => amount))
-    .attr('transform', (d, i) => `translate(${i * barWidth}, ${height})  scale(1, -1)`)
-    .on('mouseover', (d) => {
-      popup.transition()
-        .duration(200)
-        .style('opacity', 0.9);
-      const rect = d3.select(d);
-      const rectDate = rect._groups[0][0][0].split('-');
-      const rectAmount = rect._groups[0][0][1].toString().split('.');
-      let amount;
-      if (rectAmount.length === 1) {
-        amount = '$' + rectAmount[0].concat('.00');
-      } else {
-        amount = '$' + rectAmount[0].concat('.' + rectAmount[1]).concat('0');
-      }
-      popup.html(`<div id='amount'>${amount} Billion</div><div id='date'>${getDate(rectDate[1])}, ${rectDate[0]}</div>`)
-        .style('left', (d3.event.pageX + 5) + 'px')
-        .style('top', (d3.event.pageY - 50) + 'px')
-    }).on('mouseout', (d) => {
-        popup.transition()
-          .duration(500)
-          .style('opacity', 0);
-    });
-
-  const description = d3.select('#container').append('div')
-    .attr('class', 'description')
-    .style('max-width', '925px')
-    .style('font-size', '12px')
-    .style('padding-left', '15px')
-    .style('text-align', 'center')
-
-  description.append("text")
-    .text(data.description);
-
 });
